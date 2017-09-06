9bf24203eb57593e21b3f44792146f5803fb4d75
vannvaughan <vann.vaughan@yahoo.com>
2017-09-06 16:21:41 -0500

    FML

--- controllers/productDetailsCtrl.js
+++ controllers/productDetailsCtrl.js
@@ -1,8 +1,6 @@
 angular.module('app')
     .controller('productDetailsCtrl', function( $scope, shopService, $stateParams ){
 
-        console.log($stateParams)
-
         $scope.getProductsId = function(){
             shopService.getProductsId($stateParams.id).then(response => {
                 $scope.product = response.data;
