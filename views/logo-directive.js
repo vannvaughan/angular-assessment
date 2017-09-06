6046231e841d4dba4583ee0b63b5e8daa0b7bb20
vannvaughan <vann.vaughan@yahoo.com>
2017-09-06 16:14:12 -0500

    ..

--- views/logo-directive.js
+++ views/logo-directive.js
@@ -1,7 +1,7 @@
 angular.module('app')
     .directive('logo', function(){
         return {
-            templateUrl: './logo.html',
+            templateUrl: '/logo.html',
             retrict: 'E'
         }
 })
