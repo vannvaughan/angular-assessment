0c4d74131ff464982888bd4d8dbe6fe79da35ad6
vannvaughan <vann.vaughan@yahoo.com>
2017-09-06 16:03:14 -0500

    Updated logo-directives

--- directives/logo-directive.js
+++ directives/logo-directive.js
@@ -1,7 +1,7 @@
 angular.module('app')
     .directive('logo', function(){
         return {
-            templateUrl: '../views/logo.html',
+            templateUrl: './views/logo.html',
             retrict: 'E'
         }
 })
