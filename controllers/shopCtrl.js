87eb179cf4a9c825e6260c0860bcf14ce89fc0ed
vannvaughan <vann.vaughan@yahoo.com>
2017-09-06 16:10:22 -0500

    yep

--- controllers/shopCtrl.js
+++ controllers/shopCtrl.js
@@ -4,7 +4,6 @@ angular.module('app')
         shopService.getProducts()
             .then(response => {
                 $scope.products = response.data
-                return response.data
         })
 
 
